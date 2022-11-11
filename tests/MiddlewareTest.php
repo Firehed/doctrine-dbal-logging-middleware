1f824365347dd9c4ff7556499df75daf243fc6af
Eric Stern <eric@ericstern.com>
2022-11-10 23:37:34 -0800

    Get closer to a useful test

--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Firehed\DbalLogger;
 
+use Doctrine\DBAL\Connection;
+use PDO;
+
 /**
- * @covers Middleware
+ * @covers Firehed\DbalLogger\Middleware
  */
 class MiddlewareTest extends \PHPUnit\Framework\TestCase
 {
@@ -19,18 +22,26 @@ class MiddlewareTest extends \PHPUnit\Framework\TestCase
 
         $c = $this->createDbal($middleware);
         // var_dump($c);
-        $c->query('SELECT 1');
+        // $c->query('SELECT 1');
+
+        $s = $c->prepare('SELECT * FROM users WHERE id = :id');
+        $s->bindValue('id', 'abcdef');
+
+        $r = $s->executeQuery();
     }
 
-    public function createDbal(Middleware $middleware)
+    public function createDbal(Middleware $middleware): Connection
     {
         $connectionParams = [
-            // 'url' => 'mysql://user:secret@localhost/mydb',
             'url' => 'sqlite:///:memory:',
         ];
         $config = new \Doctrine\DBAL\Configuration();
         $config->setMiddlewares([$middleware]);
         $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+        $pdo = $conn->getWrappedConnection()->getNativeConnection();
+        assert($pdo instanceof PDO);
+        $pdo->exec('CREATE TABLE users (id string PRIMARY KEY);');
 
         return $conn;
     }
