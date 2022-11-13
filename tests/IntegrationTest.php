69ee94be68a3ba093e4a5e1411f5b136cd5e5080
Eric Stern <eric@ericstern.com>
2022-11-13 07:00:05 -0800

    Commit/Rollback tests

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use PDO;
+use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * @group integration
@@ -73,5 +74,56 @@ class IntegrationTest extends \PHPUnit\Framework\TestCase
         $pdo->exec('CREATE TABLE users (id string PRIMARY KEY);');
 
         return $conn;
+    }
+
+    /**
+     * @dataProvider loggers
+     */
+    public function testCommit(MockObject&QueryLogger $logger): void
+    {
+        $logger->expects(self::exactly(3))
+            ->method('startQuery')
+            ->withConsecutive(
+                ['START TRANSACTION', null, null],
+                ['INSERT INTO users (id) VALUES (:id)', ['id' => 'abc'], ['id' => 2]],
+                ['COMMIT', null, null],
+            );
+        $conn = $this->createDbal($logger);
+        self::assertTrue($conn->beginTransaction());
+        $stmt = $conn->prepare('INSERT INTO users (id) VALUES (:id)');
+        $stmt->bindValue('id', 'abc');
+        self::assertSame(1, $stmt->executeStatement());
+        self::assertTrue($conn->commit());
+    }
+
+    /**
+     * @dataProvider loggers
+     */
+    public function testRollback(MockObject&QueryLogger $logger): void
+    {
+        $logger->expects(self::exactly(3))
+            ->method('startQuery')
+            ->withConsecutive(
+                ['START TRANSACTION', null, null],
+                ['INSERT INTO users (id) VALUES (:id)', ['id' => 'abc'], ['id' => 2]],
+                ['ROLLBACK', null, null],
+            );
+        $conn = $this->createDbal($logger);
+        self::assertTrue($conn->beginTransaction());
+        $stmt = $conn->prepare('INSERT INTO users (id) VALUES (:id)');
+        $stmt->bindValue('id', 'abc');
+        self::assertSame(1, $stmt->executeStatement());
+        self::assertTrue($conn->rollback());
+    }
+
+    /**
+     * @return array{MockObject}[]
+     */
+    public function loggers(): array
+    {
+        return [
+            'QueryLogger' => [self::createMock(QueryLogger::class)],
+            'DbalLogger' => [self::createMock(DbalLogger::class)],
+        ];
     }
 }
